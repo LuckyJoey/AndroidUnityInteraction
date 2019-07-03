c57520eb1988f61e87732c9658fd0bd6da24071b
Joey <luckyjoeyma@gmail.com>
2019-07-03 17:04:05 +0800

    Mark

--- AndroidStudioProject/MyApplication/mylibrary/src/main/java/com/example/mylibrary/MainActivity.java
+++ AndroidStudioProject/MyApplication/mylibrary/src/main/java/com/example/mylibrary/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.unity3d.player.UnityPlayerActivity;
 
-public class MainActivity extends UnityPlayerActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
